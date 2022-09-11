af9549446aa076dca5229b19a9291d596d75e394
Tristan Gauci <16343865+Maega@users.noreply.github.com>
2022-09-11 17:26:33 +1000

    Create Bypass The Age Paywall.js

---
+++ Bypass The Age Paywall.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name        Bypass The Age Paywall
+// @description Bypasses The Age news paywall
+// @match       https://www.theage.com.au/*
+// ==/UserScript==
+
+localStorage.removeItem('ffx:paywallCount');
