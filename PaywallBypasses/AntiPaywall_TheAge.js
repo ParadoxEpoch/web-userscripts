b7c835d22ce681753744def660472ce046c27f75
Tristan Gauci <tristan@maega.com.au>
2022-10-26 17:34:42 +1100

    Added URL state change observer to The Age bypass

--- PaywallBypasses/AntiPaywall_TheAge.js
+++ PaywallBypasses/AntiPaywall_TheAge.js
@@ -7,11 +7,40 @@
 // @icon        https://www.google.com/s2/favicons?sz=64&domain=theage.com.au
 // ==/UserScript==
 
-// Fetch the current ffx:paywallCount object from localStorage so we can perform a check on it later on.
-const paywallCount = localStorage.getItem('ffx:paywallCount');
+// * This init function contains the routine to remove the paywallCount object from localStorage.
+function init() {
+    // Fetch the current ffx:paywallCount object from localStorage so we can perform a check on it later on.
+    let paywallCount = localStorage.getItem('ffx:paywallCount');
 
-// Remove the ffx:paywallCount object from localStorage.
-localStorage.removeItem('ffx:paywallCount');
+    // Remove the ffx:paywallCount object from localStorage.
+    localStorage.removeItem('ffx:paywallCount');
 
-// If the paywallCount had already exceeded the free article limit, reload the page after clearing the counter.
-if (JSON.parse(paywallCount).value > 2) location.reload();
+    // If the paywallCount had already exceeded the free article limit, reload the page after clearing the counter.
+    if (!!paywallCount && JSON.parse(paywallCount).value > 2) location.reload();
+}
+
+let oldHref = document.location.href;
+
+// * Create a MutationObserver instance on page load to watch for URL state changes.
+// ? We're doing this because The Age uses the history API for page navigation and we need a way to trigger script execution on each article load.
+window.onload = function () {
+    const bodyList = document.querySelector("body")
+
+    const observer = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+            if (oldHref != document.location.href) {
+                oldHref = document.location.href;
+                init();
+            }
+        });
+    });
+
+    const config = {
+        childList: true,
+        subtree: true
+    };
+
+    observer.observe(bodyList, config);
+};
+
+init();
